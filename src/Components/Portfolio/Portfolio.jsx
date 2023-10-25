15578cc2a39dd7b1773646c3092eb90fc2f59909
DiegoDalera <dmdalera@gmail.com>
2023-10-25 08:45:17 -0300

    proyect base

--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -1,18 +1,61 @@
 import React from "react";
 import "./Portfolio.css";
-import Card from "./Card";
+import anto from "../../assets/proyectos/Anto.jpg";
+import Card_p from "./Card_p";
 
 const Portfolio = () => {
   return (
     <div>
       <h2>My Proyects</h2>
+      
       <div className="cards">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        <Card_p
+          thumb={anto}
+          title={"texto"}
+          text={"texto descripcion"}
+          demoLink={"https://antoslashes.vercel.app/"}
+          sourceLink={"https://github.com/DiegoDalera/turneroEstetica"}
+        />
+
+        <Card_p
+          thumb={anto}
+          title={"texto"}
+          text={"texto descripcion"}
+          demoLink={"https://antoslashes.vercel.app/"}
+          sourceLink={"https://github.com/DiegoDalera/turneroEstetica"}
+        />
+
+        <Card_p
+          thumb={anto}
+          title={"texto"}
+          text={"texto descripcion"}
+          demoLink={"https://antoslashes.vercel.app/"}
+          sourceLink={"https://github.com/DiegoDalera/turneroEstetica"}
+        />
+
+        <Card_p
+          thumb={anto}
+          title={"texto"}
+          text={"texto descripcion"}
+          demoLink={"https://antoslashes.vercel.app/"}
+          sourceLink={"https://github.com/DiegoDalera/turneroEstetica"}
+        />
+
+        <Card_p
+          thumb={anto}
+          title={"texto"}
+          text={"texto descripcion"}
+          demoLink={"https://antoslashes.vercel.app/"}
+          sourceLink={"https://github.com/DiegoDalera/turneroEstetica"}
+        />
+
+        <Card_p
+          thumb={anto}
+          title={"texto"}
+          text={"texto descripcion"}
+          demoLink={"https://antoslashes.vercel.app/"}
+          sourceLink={"https://github.com/DiegoDalera/turneroEstetica"}
+        />
       </div>
     </div>
   );
