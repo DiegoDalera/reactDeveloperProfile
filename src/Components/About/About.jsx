d80779412c97fd27d6e23730d315213464a27a79
DiegoDalera <dmdalera@gmail.com>
2023-10-25 08:00:22 -0300

    experriencia

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -9,7 +9,7 @@ function About() {
 
       <div className="experiencia">
         <img src={MiImagen} alt="Diego Dalera" className="img-experiencia" />
-        <div className="exp-description">
+        <div className="desc-experiencia">
           <p>
             Soy desarrollador web y licenciado en gestión educativa con más de
             20 años de experiencia en el campo de la tecnología y la educación.
@@ -26,6 +26,23 @@ function About() {
             empece a trabajar como desarrollador web independiente, creando
             sitios web para pequeñas empresas y organizaciones no
             gubernamentales.
+          </p>
+          <p>
+            Después de graduarme, comencé a trabajar como profesor de tecnología
+            y diseño en una escuela secundaria. Al mismo tiempo, empece a
+            trabajar como desarrollador web independiente, creando sitios web
+            para pequeñas empresas y organizaciones no gubernamentales.
+          </p>
+          <p>
+            A lo largo de los años, me he mantenido actualizado en las últimas
+            tendencias y tecnologías en el campo del desarrollo web. Ha asistido
+            a numerosos cursos y conferencias, y ha obtenido certificaciones en
+            diversas áreas relacionadas con la tecnología y la educación. Hoy en
+            día, a mis 52 años,sigo trabajando como desarrollador web y asesor
+            de tecnología, y sigue teniendo una gran pasión por la educación.
+            También se ha convertido en un mentor para muchos jóvenes que
+            quieren seguir una carrera en él campo de la tecnología, y ha
+            ayudado a muchos a encontrar su camino en este emocionante campo.
           </p>
         </div>
       </div>
